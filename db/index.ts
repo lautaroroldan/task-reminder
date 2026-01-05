677b32db2fb0068de59a00d932ba454add57e7d3
Lautaro <lautaronicolasroldan@gmail.com>
2026-01-05 04:57:59 -0300

    Fix and add env

--- db/index.ts
+++ db/index.ts
@@ -3,6 +3,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 config({ path: '.env' }); // or .env.local
+const connectionString = process.env.DATABASE_URL!;
 
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(connectionString, { prepare: false });
 export const db = drizzle({ client });
